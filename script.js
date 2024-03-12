fbb37a8d9c274209fecf1af5fc0d2eaaa81e0be9
Cameron Laury <24402409+Cammylaury@users.noreply.github.com>
2024-03-12 12:47:02 -0500

    Added pseudo-code for JS

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello World")
+
+function getComputerChoice() {
+    // Randomly return the computer choice of rock, paper, or scissors
+
+
+}
+
+function playRound(computerSelection, playerSelection) {
+    // add functionality and logic to what a round of RPS is.
+    // Retrieve the player selection and the computer selection to determine a winner
+    // Add win/tie/lose state and message
+    // Make input case-insensitive so RoCk, rock, and ROCK are all valid answers
+
+    return(computerSelection, playerSelection)
+
+}
